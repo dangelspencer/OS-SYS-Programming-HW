46f186afdcd3601a2f78a86220f420941e0720da
Spencer <dangelspencer@gmail.com>
2016-09-19 21:21:27 -0400

    Part 1 completed

--- HW1/part1.cpp
+++ HW1/part1.cpp
@@ -13,26 +13,27 @@ int main(int argc, char* argv[]){
     return 0;
   }
   
-  int PID;
-  int data = 10;
+  pid_t PID;
   int N = atoi(argv[1]);
-  
-  for (int i = 0; i < 3; i++){
-    PID = fork();
-  }
-  
-  if (PID == 0){
-    //parent process 
-    for (int i = 0; i < N; i++){
-      printf("This is the main process, my PID is %d\n", PID);
-      sleep(2);
-    }
-  } else {
-    //child process
-    for (int i = 0; i < N; i++){
-      printf("This is a child process, my PID is %d, my parent PID is %d\n", PID, getppid());
-    }
+
+  for (int i = 0; i < N; i++){
+    printf("This is the main process, my PID is %d\n", getpid());
+    sleep(2);
   }
 
+  for (int i = 0; i < 3; i++){
+    PID = fork();
+    
+    if (PID){
+      break;
+    }
+    else{
+      for (int i = 0; i < N; i++){
+        printf("This is a child process, my PID is %d, my parent PID is %d\n", getpid(), getppid());
+        sleep(2);
+      }
+    }
+  }
+  
   return 0;
 }
