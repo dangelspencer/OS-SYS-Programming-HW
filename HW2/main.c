42571fff3d5ff7200fceaccd4928fcb7b3448f92
dangelspencer <dangelspencer@gmail.com>
2016-10-14 21:33:04 -0400

    more updates

--- HW2/main.c
+++ HW2/main.c
@@ -7,53 +7,45 @@
 #include <sys/times.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/sem.h>
+#include <sys/wait.h> 
 
 #define FILE_SIZE 11
-#define NO_PROC 10
+#define NO_PROC   10
 
 int DelayCount = 0;
 
 int readerID = 0;
 int writerID = 0;
 
-char *shared_buffer;
+char* shared_buffer;
 
 int Delay100ms = 0;
-
-int semid;
-
-int num_readers;
 
 /*-------------------------------------------
     Delay routines
  --------------------------------------------*/
 void basic_delay()
 {
-    long i, j, k;
-    for (i = 0; i < 200L; i++)
-    {
-        for (j = 0; j < 400L; j++)
-        {
-            k = k + i;
-        }
-    }
+   long i,j,k;
+   for (i=0;i<200L;i++) {
+       for (j=0;j<400L;j++) {
+          k = k+i;
+       }
+   }
 }
 
 /* do some delays (in 100 ms/tick) */
 void delay(int delay_time)
 {
-    int i, j;
+   int i,j;
 
-    for (i = 0; i < delay_time; i++)
-    {
-        for (j = 0; j < Delay100ms; j++)
-        {
-            basic_delay();
-        }
-    }
+   for (i=0; i<delay_time; i++) {
+      for (j=0;j<Delay100ms;j++) {
+          basic_delay();
+      }
+   }
 }
+
 
 int stop_alarm = 0;
 
@@ -63,6 +55,7 @@ sig_alrm(int signo)
     stop_alarm = 1;
 }
 
+
 /*------------------------------------------
  * Since the speed of differet systems vary,
  * we need to calculate the delay factor 
@@ -71,114 +64,74 @@ sig_alrm(int signo)
 void calcuate_delay()
 {
     int i;
-    struct tms t1;
-    struct tms t2;
+    struct  tms t1;
+    struct  tms t2;
     clock_t t;
-    long clktck;
-    double td;
+    long    clktck;
+    double  td;
 
     printf(".... Calculating delay factor ......\n");
     stop_alarm = 0;
     if (signal(SIGALRM, sig_alrm) == SIG_ERR)
-        perror("Set SIGALRM");
-    alarm(5); /* stop the following loop after 5 seconds */
+       perror("Set SIGALRM");
+    alarm(5);  /* stop the following loop after 5 seconds */
 
     times(&t1);
-    while (stop_alarm == 0)
-    {
+    while (stop_alarm == 0) {
         DelayCount++;
         basic_delay();
-    }
+    }    
     times(&t2);
-    alarm(0); /* turn off the timer */
+    alarm(0);   /* turn off the timer */
 
     /* Calcluate CPU time */
-    t = t2.tms_utime - t1.tms_utime;
+    t = t2.tms_utime - t1.tms_utime; 
 
     /* fetch clock ticks per second */
-    if ((clktck = sysconf(_SC_CLK_TCK)) < 0)
-        perror("sysconf error");
+    if ( (clktck = sysconf(_SC_CLK_TCK)) < 0 )
+       perror("sysconf error");
 
     /* actual delay in seconds */
     td = t / (double)clktck;
-
-    Delay100ms = DelayCount / td / 10;
+    
+    Delay100ms = DelayCount/td/10;
 
     if (Delay100ms == 0)
-        Delay100ms++;
+       Delay100ms++;
 
     printf(".... End calculating delay factor\n");
 }
+
+
+
 
 /*-------------------------------------------
    The reader
  --------------------------------------------*/
 void reader()
 {
-    struct sembuf op1;
-    //lock reading semaphore
-    op1.sem_num = 0; 
-    op1.sem_op = -1; 
-    op1.sem_flg = 0; 
-    if (semop(semid,&op1,1) == -1)
-    {
-      perror("Thread1:semop failure Reason:");
-      exit(-1);
-    }
-    
-    
-    
-    int i, j, n;
-    char results[FILE_SIZE];
+  int i,j,n;
+  char results[FILE_SIZE];
 
-    srand(2);
-    for (i = 0; i < 1; i++)
-    {
-        printf("Reader %d (pid = %d) arrives\n", readerID, getpid());
+  srand(2);
+  for (i=0; i<1; i++) {
+      printf("Reader %d (pid = %d) arrives\n", readerID, getpid()); 
 
-        /* read data from shared data */
-        for (j = 0; j < FILE_SIZE; j++)
-        {
-            results[j] = shared_buffer[j];
-            delay(4);
-        }
-
-        /* display result */
-        results[j] = 0;
-        printf("      Reader %d gets results = %s\n",
-               readerID, results);
-    }
-
-    //unlock reading semaphore
-    op1.sem_num = 0; 
-    op1.sem_op = 1; 
-    op1.sem_flg = 0; 
-    if (semop(semid,&op1,1) == -1)
-    {
-      perror("Thread1:semop failure Reason:");
-      exit(-1);
-    }
-    
-    num_readers--;
-    
-    if (num_readers == 0)
-    {
-      typedef union semun {
-          int val;
-          struct semid_ds *buf;
-          ushort *array;
-      } semun_t;
-      semun_t arg, arg1;
-    
-      //reading semaphore
-      arg1.val = 0;
-      if (semctl(semid, 0, SETVAL, arg1) < 0)
-      {
-          perror("semctl failure Reason: ");
-          exit(-1);
+      /* read data from shared data */
+      for (j=0; j<FILE_SIZE; j++) {
+         results[j] = shared_buffer[j]; 
+         delay(4);  
       }
-    }
+ 
+      /* display result */
+      results[j] = 0;
+      printf("      Reader %d gets results = %s\n", 
+              readerID, results);
+  }
 }
+
+
+
 
 /*-------------------------------------------
    The writer. It tries to fill the buffer
@@ -186,54 +139,30 @@ void reader()
  --------------------------------------------*/
 void writer()
 {
-    struct sembuf op1;
+  int i,j,n;
+  char data[FILE_SIZE];
 
-    //lock the semaphore
-    op1.sem_num = 0;
-    op1.sem_op = -1;
-    op1.sem_flg = 0;
-    if (semop(semid, &op1, 1) == -1)
-    {
-        perror("Thread1:semop failure Reason:");
-        exit(-1);
-    }
+  srand(1);
 
-    int i, j, n;
-    char data[FILE_SIZE];
+  for (j=0; j<FILE_SIZE-1; j++) {
+      data[j]= writerID + '0';
+  }
+  data[j]= 0;
 
-    srand(1);
+  for (i=0; i<1; i++) {
+      printf("Writer %d (pid = %d) arrives, writing %s to buffer\n", 
+              writerID, getpid(), data);
 
-    for (j = 0; j < FILE_SIZE - 1; j++)
-    {
-        data[j] = writerID + '0';
-    }
-    data[j] = 0;
+      /* write to shared buffer */
+      for (j=0; j<FILE_SIZE-1; j++) {
+          shared_buffer[j]= data[j]; 
+          delay(3);  
+      }
 
-    for (i = 0; i < 1; i++)
-    {
-        printf("Writer %d (pid = %d) arrives, writing %s to buffer\n",
-               writerID, getpid(), data);
-
-        /* write to shared buffer */
-        for (j = 0; j < FILE_SIZE - 1; j++)
-        {
-            shared_buffer[j] = data[j];
-            delay(3);
-        }
-
-        printf("Writer %d finishes\n", writerID);
-    }
-
-    //unlock writing semaphore
-    op1.sem_num = 0; 
-    op1.sem_op = 1; 
-    op1.sem_flg = 0; 
-    if (semop(semid,&op1,1) == -1)
-    {
-      perror("Thread1:semop failure Reason:");
-      exit(-1);
-    }
+      printf("Writer %d finishes\n", writerID);
+  }
 }
+
 
 /*-------------------------------------------
 
@@ -242,92 +171,38 @@ void writer()
 *-------------------------------------------*/
 void create_reader()
 {
-
-    num_readers++;
-
-    if (0 == fork())
-    {
+    if (0 == fork()) {
         reader();
         exit(0);
     }
 
     readerID++;
-    
 }
 
 void create_writer()
 {
-    if (0 == fork())
-    {
+    if (0 == fork()) {
         writer();
         exit(0);
     }
 
     writerID++;
-    
-    typedef union semun {
-        int val;
-        struct semid_ds *buf;
-        ushort *array;
-    } semun_t;
-    semun_t arg, arg1;
-    
-    //writing semaphore
-    arg.val = 0;
-    if (semctl(semid, 0, SETVAL, arg) < 0)
-    {
-        perror("semctl failure Reason:");
-        exit(-1);
-    }
 }
+
+
 
 /*-------------------------------------------
  --------------------------------------------*/
-void main()
-{
-    int return_value;
-    char InitData[] = "0000000000\n";
-    int i;
-    int fd;
+void main() 
+{ 
+  int return_value;
+  char InitData[]="0000000000\n";
+  int i;
+  int fd;
 
-    num_readers = 0;    
-    
-    //initialize semaphores
-    typedef union semun {
-        int val;
-        struct semid_ds *buf;
-        ushort *array;
-    } semun_t;
-    semun_t arg, arg1;
-    semid = semget(IPC_PRIVATE, 2, 0666 | IPC_CREAT);
-    
-    //writing semaphore
-    arg.val = 1;
-    if (semctl(semid, 0, SETVAL, arg) < 0)
-    {
-        perror("semctl failure Reason:");
-        exit(-1);
-    }
+  calcuate_delay();
 
-    //writing semaphore
-/*    arg.val = 1;
-    if (semctl(semid, 0, SETVAL, arg) < 0)
-    {
-        perror("semctl failure Reason:");
-        exit(-1);
-    }*/
-
-/*    //reading semaphore
-    arg1.val = 1;
-    if (semctl(semid, 1, SETVAL, arg1) < 0)
-    {
-        perror("semctl failure Reason: ");
-        exit(-1);
-    }*/
-
-    calcuate_delay();
-
-    /*-------------------------------------------------------
+  /*-------------------------------------------------------
    
        The following code segment creates a memory
      region shared by all child processes
@@ -335,67 +210,66 @@ void main()
      worry. You don't have to understand the detail
      of mmap() to finish the homework
   
-  -------------------------------------------------------*/
+  -------------------------------------------------------*/ 
 
-    fd = open("race.dat", O_RDWR | O_CREAT | O_TRUNC, 0600);
-    if (fd < 0)
-    {
-        perror("race.dat ");
-        exit(1);
-    }
+  fd = open("race.dat", O_RDWR | O_CREAT | O_TRUNC, 0600);
+  if ( fd < 0 ) {
+     perror("race.dat ");
+     exit(1);
+  }
 
-    write(fd, InitData, FILE_SIZE);
+  write(fd, InitData, FILE_SIZE);
 
-    unlink("race.dat");
+  unlink("race.dat");
 
-    shared_buffer = mmap(0, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (shared_buffer == (caddr_t)-1)
-    {
-        perror("mmap");
-        exit(2);
-    }
+  shared_buffer = mmap(0, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if ( shared_buffer == (caddr_t) -1) {
+     perror("mmap");
+     exit(2);
+  }
+     
 
-    /*------------------------------------------------------- 
+  /*------------------------------------------------------- 
   
        Create some readers and writes (processes)
   
-  -------------------------------------------------------*/
-    create_reader();
-    delay(1);
-    create_writer();
-    delay(1);
-    create_reader();
-    create_reader();
-    create_reader();
-    delay(1);
-    create_writer();
-    delay(1);
-    create_reader();
+  -------------------------------------------------------*/ 
+  create_reader();
+  delay(1);
+  create_writer();
+  delay(1);
+  create_reader();
+  create_reader();
+  create_reader();
+  delay(1);
+  create_writer();
+  delay(1);
+  create_reader();
 
-    /* delay 15 seconds so all previous readers/writes can finish.
+  /* delay 15 seconds so all previous readers/writes can finish.
    * This is to prevent writer starvation
    */
-    delay(150);
+  delay(150);
 
-    create_writer();
-    delay(1);
-    create_writer();
-    delay(1);
-    create_reader();
-    create_reader();
-    create_reader();
-    delay(1);
-    create_writer();
-    delay(1);
-    create_reader();
 
-    /*------------------------------------------------------- 
+  create_writer();
+  delay(1);
+  create_writer();
+  delay(1);
+  create_reader();
+  create_reader();
+  create_reader();
+  delay(1);
+  create_writer();
+  delay(1);
+  create_reader();
+  
+  /*------------------------------------------------------- 
   
       Wait until all children terminate
   
   --------------------------------------------------------*/
-    for (i = 0; i < (readerID + writerID); i++)
-    {
-        wait(NULL);
-    }
+  for (i=0; i<(readerID+writerID); i++) {
+      wait(NULL);
+  }
 }
