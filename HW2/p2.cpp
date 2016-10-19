e643b77516af7a8ab46f7ff36588b0d1d19ece19
dangelspencer <dangelspencer@gmail.com>
2016-10-19 08:22:59 -0400

    updated p2 with code from stackoverflow

--- HW2/p2.cpp
+++ HW2/p2.cpp
@@ -1,20 +1,39 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<iostream>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-using namespace std;
+int main(int argc, char *argv[])
+{
+    int fd[2];
+    int val = 0;
 
-int main(int argc, char* argv[]){
-  for (int i = 0; i < 5; i++){
-    
-    if (fork() == 0){
-      printf("This is a child process, my PID is %d, my parent PID is %d\n", getpid(), getppid());
-      break;
+    // create pipe descriptors
+    pipe(fd);
+
+    // fork() returns 0 for child process, child-pid for parent process.
+    if (fork() != 0)
+    {
+        // parent: writing only, so close read-descriptor.
+        close(fd[0]);
+
+        // send the value on the write-descriptor.
+        val = 100;
+        write(fd[1], &val, sizeof(val));
+        printf("Parent(%d) send value: %d\n", getpid(), val);
+
+        // close the write descriptor
+        close(fd[1]);
     }
-  }
-  //small pause for all child processes to finish (not required for the program to run correctly)
-  sleep(1);
+    else
+    {   // child: reading only, so close the write-descriptor
+        close(fd[1]);
+
+        // now read the data (will block)
+        read(fd[0], &val, sizeof(val));
+        printf("Child(%d) received value: %d\n", getpid(), val);
+
+        // close the read-descriptor
+        close(fd[0]);
+    }
+    return 0;
 }
