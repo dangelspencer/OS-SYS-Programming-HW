87a75431fd7df90bd657a25183549896874f910e
Spencer <dangelspencer@gmail.com>
2016-10-31 19:51:19 -0400

    Program takes in edges and prints out a matrix

--- Algorithms/HW3/HW3.cpp
+++ Algorithms/HW3/HW3.cpp
@@ -1,18 +1,64 @@
 #include<iostream>
+#include<stdio.h>
 
 using namespace std;
 
+void ZeroMatrix(int* array, int n);
+void PrintMatrix(int* array, int n);
+
 int main() {
-  while( x!=0)
-  {
-  cin>>x;
-  result=x + result;
+    cout << "Please enter the number of vertices: ";
+    int n;
+    cin >> n;
+    
+    int a[n*n];
+    int d[n*n];
+    
+    int node1, node2;
+    bool working = true;
+    
+    ZeroMatrix(a, n);
+    ZeroMatrix(d, n);
+    
+    while (working) {
+        cout << "Enter first node of edge: ";
+        cin >> node1;
+        if (node1 < 0) {
+            working = false;
+            break;
+        }
+        
+        cout << "Enter second node of edge: ";
+        cin >> node2;
+        
+        a[node1 * n + node2] = 1;
+        a[node2 * n + node1] = 1;
+    }
+    
+    PrintMatrix(a, n);
+    
+    return 0;
+}
 
-  }
-  if(x==0){
+void ZeroMatrix(int* array, int n) {
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+            array[x*n+y] = 0;
+        }
+    }
+}
 
-
-  cout<<result<<endl;
-
-  }
+void PrintMatrix(int* array, int n) {
+    cout << "  ";
+    for (int i = 0; i < n; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+    for (int y = 0; y < n; y++) {
+        cout << y << " ";
+        for (int x = 0; x < n; x++){
+            cout << array[x*n+y] << " ";
+        }
+        cout << endl;
+    }
 }
